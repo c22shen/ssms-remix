3c67bcee7a43484f350f4ca67310679b42e7cafa
Xiao Shen <xiaoshen2009@gmail.com>
2016-12-12 11:46:49 -0500

    material design

--- public/javascripts/angular-app/directives/bar-directive.js
+++ public/javascripts/angular-app/directives/bar-directive.js
@@ -5,13 +5,13 @@ angular.module('app').directive('barChart', ['d3', '$rootScope', 'myConfig',
         return {
             restrict: 'E',
             compile: function(elements, attrs, transclude) {
-                var svg = d3.select(elements[0]).append('svg').attr('width', "100%").attr('height', "30");
+                var svg = d3.select(elements[0]).append('svg').attr('width', "100%").attr('height', "10");
                 // var entry = d3.select(elements[0]).append("div")
                 // 	.attr("class", "text-center")
                 // 	.style("color", "#2980B9")
                 // 	.html("<i class='fa fa-arrow-up fa-3x' aria-hidden='true' style='color: #2980B9'></i>");
                  
-                svg.append('g').attr("class","background").append('rect').attr('height', 10).attr('width', "100%").style('fill', "#8ccdfd");
+                svg.append('g').attr("class","background").append('rect').attr('height', 10).attr('width', "100%").style('fill', "#303030");
                 svg.append('g').attr("class", "info").append('rect').attr('height', 10).attr('width', 0	).style('fill', "#199cfc")
                
                 //this is the link function
--- public/javascripts/angular-app/directives/schedule-directive.js
+++ public/javascripts/angular-app/directives/schedule-directive.js
@@ -7,36 +7,50 @@ angular.module('app').directive('schedule', ['myConfig',
             templateUrl: "/javascripts/angular-app/tpl/navbar.tpl.html",
             link: function postLink(scope, iElement, iAttrs) {
                 var timeAvailable = {
-                    0: {open: {hour: 0, minute: 0}, 
-                        close: {hour: 0, minute: 0}},
-                    1: {open: {hour: 8, minute: 30}, 
-                        close: {hour: 21, minute: 0}},
-                    2: {open: {hour: 8, minute: 30}, 
-                        close: {hour: 21, minute: 0}},
-                    3: {open: {hour: 8, minute: 30}, 
-                        close: {hour: 21, minute: 0}},
-                    4: {open: {hour: 8, minute: 30}, 
-                        close: {hour: 21, minute: 0}},
-                    5: {open: {hour: 8, minute: 30}, 
-                        close: {hour: 21, minute: 0}},
-                    6: {open: {hour: 10, minute: 0}, 
-                        close: {hour: 17, minute: 0}}
-            }
+                    0: {
+                        open: { hour: 0, minute: 0 },
+                        close: { hour: 0, minute: 0 }
+                    },
+                    1: {
+                        open: { hour: 8, minute: 30 },
+                        close: { hour: 16, minute: 30 }
+                    },
+                    2: {
+                        open: { hour: 8, minute: 30 },
+                        close: { hour: 21, minute: 0 }
+                    },
+                    3: {
+                        open: { hour: 8, minute: 30 },
+                        close: { hour: 21, minute: 0 }
+                    },
+                    4: {
+                        open: { hour: 8, minute: 30 },
+                        close: { hour: 21, minute: 0 }
+                    },
+                    5: {
+                        open: { hour: 8, minute: 30 },
+                        close: { hour: 21, minute: 0 }
+                    },
+                    6: {
+                        open: { hour: 10, minute: 0 },
+                        close: { hour: 17, minute: 0 }
+                    }
+                }
 
 
 
 
 
 
-// Hours: 
+                // Hours: 
 
-// Sunday  Closed
-// Monday  8:30AM–9PM
-// Tuesday 8:30AM–9PM
-// Wednesday   8:30AM–9PM
-// Thursday    8:30AM–9PM
-// Friday  8:30AM–9PM
-// Saturday    10AM–5PM
+                // Sunday  Closed
+                // Monday  8:30AM–4:30PM
+                // Tuesday 8:30AM–9PM
+                // Wednesday   8:30AM–9PM
+                // Thursday    8:30AM–9PM
+                // Friday  8:30AM–9PM
+                // Saturday    10AM–5PM
 
 
 
@@ -54,7 +68,7 @@ angular.module('app').directive('schedule', ['myConfig',
                 var endMinute = timeAvailable[weekDay].close.minute;
 
 
-           
+
 
 
                 var closeHour = timeAvailable[weekDay].close.hour;
@@ -62,23 +76,29 @@ angular.module('app').directive('schedule', ['myConfig',
 
 
                 var startTime = moment({
-                      h: startHour,
-                      m: startMinute
-                    });
+                    h: startHour,
+                    m: startMinute
+                });
 
 
 
 
                 var closeTime = moment({
-                      h: closeHour,
-                      m: closeMinute
-                    });
+                    h: closeHour,
+                    m: closeMinute
+                });
 
-                scope.startTime = startTime.format("h:mm A");
-                scope.closeTime = closeTime.format("h:mm A");
-                
-                scope.storeAvailable  = moment().isBetween(startTime, closeTime);
-                scope.statusString = scope.storeAvailable ? "Now Open": "Closed";
+                var startTime = startTime.format("h:mm A");
+                var closeTime = closeTime.format("h:mm A");
+
+
+                scope.storeAvailable = moment().isBetween(startTime, closeTime);
+                scope.statusString = scope.storeAvailable ? "NOW OPEN" : "NOW CLOSED";
+                if (startTime === closeTime) {
+                    scope.timeRange = "Closed for Sunday";
+                } else {
+                    scope.timeRange = startTime + " - " + closeTime;
+                }
             }
         }
     }
